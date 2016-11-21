85d1f53b45616ec5121ca06e275ad9f7dac471e1
l.juenemann <juenemann.lukas@gmail.com>
2016-11-21 22:59:49 +0100

    trying to fix jenkins build

--- karma.conf.js
+++ karma.conf.js
@@ -73,7 +73,14 @@ module.exports = function(config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chromium'],
+        browsers: ['ChromeNoSandbox'],
+
+		customLaunchers: {
+			ChromeNoSandbox: {
+				base: 'Chrome',
+				flags: ['--no-sandbox']
+			}
+		},
 
 
         // Continuous Integration mode
