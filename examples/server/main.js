dace8958f657589134da875d2f3053050efbc075
Pedro Scaff <pedrinho.scaff@gmail.com>
2016-12-19 12:48:33 +0100

    add new session function

--- examples/server/main.js
+++ examples/server/main.js
@@ -1,4 +1,13 @@
 'use strict';
 /* global Drawr */
 
-let server = new Drawr.ServerConnection();
+let server = new Drawr.ServerConnection({
+    name: 'pedro',
+    id: '0'
+});
+
+server.eventEmitter.on('connected', () => {
+    server.newSession('competitive lumberjack', (data) => {
+        console.log(data);
+    });
+});
--- src/js/ServerConnection.js
+++ src/js/ServerConnection.js
@@ -6,36 +6,74 @@ let EventEmitter = require('eventemitter3');
 function onWebSocketOpen() {
     console.log('connected to server');
     if (this.wsClient.readyState === this.wsClient.OPEN) {
-        this.wsClient.send(
-            JSON.stringify({type: 'hello world', data: 'it\'s me'})
-        );
+        this.eventEmitter.emit('connected');
     }
 }
 
 function onWebSocketMessage(event) {
     let msg = JSON.parse(event.data);
-    if (msg.type === 'newSession') {
-        //
-    } else if (msg.type === 'joinSession') {
-        this.eventEmitter.emit('joinSession', msg);
+    console.log(msg);
+    if (msg.type === 'ack-session') {
+        if (msg.status === 'new-session-success') {
+
+            this.initSession(msg.data);
+        }
     } else if (msg.type === 'newPeer') {
         this.eventEmitter.emit('newPeer', msg);
     }
 }
 
-let ServerConnection = function() {
+let ServerConnection = function(user) {
+    this.user = user;
     this.eventEmitter = new EventEmitter();
     // this.wsClient = new W3CWebSocket();
-    console.log('hi');
-    this.wsClient = new WebSocket('ws://localhost:8080/ws');
+    this.wsClient = new WebSocket('ws://rmbp.lan:8080/ws');
     this.wsClient.onopen = onWebSocketOpen.bind(this);
     this.wsClient.onmessage = onWebSocketMessage.bind(this);
     this.wsClient.onerror = this.onWebSocketError;
-    // this.wsClient.connect('ws://localhost:8080/ws');
+    this.session = {};
+    // this.wsClient.connect('ws://rmbp.lan:8080/ws');
+};
+
+ServerConnection.prototype.initSession = function(sessionData) {
+    this.session.id = sessionData.sessionId;
+    // this.session.users = sessionData.users;
+    // this.eventEmitter.emit('new-session-connection', session);
 };
 
 ServerConnection.prototype.onWebSocketError = function() {
     console.log('error connecting to server');
+};
+
+ServerConnection.prototype.joinSession = function(sessionId) {
+    this.wsClient.send(
+        JSON.stringify({
+            type: 'join-session',
+            data: {
+                username: this.user.name,
+                sessionId: sessionId
+            }
+        })
+    );
+};
+
+ServerConnection.prototype.newSession = function(name, callback) {
+    if (callback) {
+        if (typeof callback !== 'function') {
+            throw new Error('callback must be a function');
+        } else {
+            this.session.clientCallback = callback;
+        }
+    }
+    this.wsClient.send(
+        JSON.stringify({
+            type: 'new-session',
+            data: {
+                username: this.user.name,
+                sessionName: name
+            }
+        })
+    );
 };
 
 module.exports = ServerConnection;
